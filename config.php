ab5139049c52d033252a8df0d93fabb13a733aaa
Mina Akram Fouad <99270144+minaakram@users.noreply.github.com>
2026-02-21 09:34:55 +0200

    Add database configuration to config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER', 'telco-subscriber-db.cf7mtgp4uxib.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'TelcoPass123'); 
+define('DB_NAME', 'telcom_db');
+?>
